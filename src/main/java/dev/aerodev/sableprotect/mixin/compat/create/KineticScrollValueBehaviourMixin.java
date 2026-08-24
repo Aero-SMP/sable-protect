ea991d0efe4a3a52bfbbd20e010d248ec2701595
GameBoy200 <maxmcgrath2011@gmail.com>
2026-08-24 14:46:22 +0100

    Added checks so interaction would only cancel if the player didn't have permissions on a claimed sable contraption

--- src/main/java/dev/aerodev/sableprotect/mixin/compat/create/KineticScrollValueBehaviourMixin.java
+++ src/main/java/dev/aerodev/sableprotect/mixin/compat/create/KineticScrollValueBehaviourMixin.java
@@ -2,11 +2,17 @@ package dev.aerodev.sableprotect.mixin.compat.create;
 
 import com.simibubi.create.content.kinetics.motor.KineticScrollValueBehaviour;
 import com.simibubi.create.foundation.blockEntity.behaviour.ValueSettingsBehaviour.ValueSettings;
+import dev.aerodev.sableprotect.claim.ClaimRole;
+import dev.aerodev.sableprotect.protection.ProtectionHelper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
 
 @Mixin(value = KineticScrollValueBehaviour.class, remap = false)
 public class KineticScrollValueBehaviourMixin {
@@ -19,17 +25,24 @@ public class KineticScrollValueBehaviourMixin {
 
     private void sableProtect$cancelKineticScrollValueBehaviour(Player player, ValueSettings valueSetting, boolean ctrlHeld, CallbackInfo ci) {
         if (ci == null) {return;}
-        ci.cancel();
-        //if (!(event.getEntity() instanceof ServerPlayer)) {return;}
 
-        //final ProtectionHelper.ClaimContext ctx = ProtectionHelper.getClaimContext(event.getLevel(), event.getPos());
-        //if (ctx == null) return;
+        if (!(player instanceof ServerPlayer)) {return;}
 
-        //if (ProtectionHelper.isAdminBypass((ServerPlayer) event.getEntity())) return;
+        final KineticScrollValueBehaviour behaviour = (KineticScrollValueBehaviour) (Object) this;
 
-        //if (ctx.claimData().isInteractionsProtected() && ctx.claimData().getRole(event.getEntity().getUUID()) == ClaimRole.DEFAULT) {
-            //ci.cancel();
-            //ProtectionHelper.sendDeniedMessage((ServerPlayer) event.getEntity());
-        //}
+        final Level level = behaviour.getWorld();
+        final BlockPos pos = behaviour.getPos();
+
+        if (level == null) {return;}
+
+        final ProtectionHelper.ClaimContext ctx = ProtectionHelper.getClaimContext(level, pos);
+        if (ctx == null) return;
+
+        if (ProtectionHelper.isAdminBypass((ServerPlayer) player)) return;
+
+        if (ctx.claimData().isInteractionsProtected() && ctx.claimData().getRole(player.getUUID()) == ClaimRole.DEFAULT) {
+            ci.cancel();
+            ProtectionHelper.sendDeniedMessage((ServerPlayer) player);
+        }
     }
 }
