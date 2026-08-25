e5258386f0383c8727acdb50ccac09bf585f142b
GameBoy200 <maxmcgrath2011@gmail.com>
2026-08-25 15:32:39 +0100

    Changed target class and method of mixin

--- src/main/java/dev/aerodev/sableprotect/mixin/sim/LinkedTypewriteBlockMixin.java
+++ src/main/java/dev/aerodev/sableprotect/mixin/sim/LinkedTypewriteBlockMixin.java
@@ -1,45 +1,40 @@
 package dev.aerodev.sableprotect.mixin.sim;
 
 import dev.aerodev.sableprotect.SableProtectMod;
+import dev.simulated_team.simulated.content.blocks.redstone.linked_typewriter.LinkedTypewriterBlock;
 import dev.simulated_team.simulated.content.blocks.redstone.linked_typewriter.LinkedTypewriterBlockEntity;
 import dev.aerodev.sableprotect.claim.ClaimRole;
 import dev.aerodev.sableprotect.protection.ProtectionHelper;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.minecraft.world.phys.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import com.mojang.logging.LogUtils;
-import org.slf4j.Logger;
 
-import java.util.UUID;
-
-@Mixin(value = LinkedTypewriterBlockEntity.class, remap = false)
-public class LinkedTypewriteBlockEntityMixin {
+@Mixin(value = LinkedTypewriterBlock.class, remap = false)
+public class LinkedTypewriteBlockMixin {
     @Inject(
-            method = "checkAndStartUsing",
+            method = "useItemOn",
             at = @At("HEAD"),
             cancellable = true,
             remap = false
     )
-    private void sableProtect$cancelsTypewriteFocus(UUID userID, CallbackInfoReturnable<Boolean> cir) {
+    private void sableProtect$cancelsTypewriteFocus(ItemStack itemStack, BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<ItemInteractionResult> cir) {
         SableProtectMod.LOGGER.info("Mixin catched!");
 
         final LinkedTypewriterBlockEntity behaviour = (LinkedTypewriterBlockEntity) (Object) this;
 
-        final Level level = behaviour.getLevel();
-
         if (level == null) {
             SableProtectMod.LOGGER.info("level = null");
         }
-
-        final Player player = level.getPlayerByUUID(userID);
 
         if (player == null) {
             SableProtectMod.LOGGER.info("Player = null");
@@ -47,7 +42,7 @@ public class LinkedTypewriteBlockEntityMixin {
 
         if (!(player instanceof ServerPlayer)) {return;}
 
-        final ProtectionHelper.ClaimContext ctx = ProtectionHelper.getClaimContext(level, behaviour.getBlockPos());
+        final ProtectionHelper.ClaimContext ctx = ProtectionHelper.getClaimContext(level, blockPos);
         if (ctx == null) return;
 
         if (ProtectionHelper.isAdminBypass((ServerPlayer) player)) return;
@@ -55,7 +50,7 @@ public class LinkedTypewriteBlockEntityMixin {
         if (ctx.claimData().isInteractionsProtected() && ctx.claimData().getRole(player.getUUID()) == ClaimRole.DEFAULT) {
             //behaviour.disconnectUser();
             SableProtectMod.LOGGER.info("Attemping to cancel focus");
-            cir.setReturnValue(false);
+            cir.setReturnValue(ItemInteractionResult.FAIL);
             ProtectionHelper.sendDeniedMessage((ServerPlayer) player);
         }
     }
